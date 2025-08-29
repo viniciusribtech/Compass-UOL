b1c3e009cf031fffc702d4f073b919706bc777f4
Vinicius Ribeiro <vinir14.p5@gmail.com>
2025-08-29 13:29:58 -0300

    correção do path no s3

--- Sprint05/Desafio/ETL.py
+++ Sprint05/Desafio/ETL.py
@@ -12,6 +12,6 @@ s3 = boto3.client(
 )
 
 s3.upload_file(arquivo1, bucket, "desafio-sprint5-vinicius/Raw/Local/CSV/Movies/2025/08/28/movies.csv")
-s3.upload_file(arquivo2, bucket, "desafio-sprint5-vinicius/Raw/Local/CSV/Movies/2025/08/28/series.csv")
+s3.upload_file(arquivo2, bucket, "desafio-sprint5-vinicius/Raw/Local/CSV/Series/2025/08/28/series.csv")
 
 print("Arquivos enviados para o bucket ", bucket) # teste
